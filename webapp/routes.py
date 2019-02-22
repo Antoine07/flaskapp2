1ec9ba37ab996777bdd283528ad78e8c1478da37
Antoine07 <antoine.lucsko@gmail.com>
2019-02-22 16:30:52 +0100

    filter example

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,6 +1,14 @@
 from webapp import app, render_template
 from webapp import models as m
 
+# @app.template_filter('avg')
+# def avg_student_filter(userId):
+#     pass
+
+@app.template_filter('cap')
+def cap_filter(txt):
+    return txt.upper()
+
 @app.route('/')
 @app.route('/home')
 def index():
